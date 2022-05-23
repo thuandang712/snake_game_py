a94526b8f112490d9324d5a8d1b4885a20fd6857
Thuan Dang <thuandang@Thuans-MacBook-Pro.local>
2022-05-23 16:49:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+# Turn off tracer
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+# control the snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.05)  # slow the snake down
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 10:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # detec collision with tail
+    # if head collides with any dots in the tail -> game over
+    for dot in snake.dots[1:]:
+        if snake.head.distance(dot) < 8:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+
+STARTING_POS = [(0, 0), (-10, 0), (-20, 0)]
+MOVING_DISTANCE = 10
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.dots = []
+        self.create_snake()
+        self.head = self.dots[0]
+
+    def create_snake(self):
+        for pos in STARTING_POS:
+            self.add_dot(pos)
+
+    def add_dot(self, pos):
+        new_dot = Turtle("square")
+        new_dot.color("white")
+        new_dot.penup()
+        new_dot.shapesize(.5, .5)
+        new_dot.goto(pos)
+        self.dots.append(new_dot)
+
+    def extend(self):
+        self.add_dot(self.dots[-1].position())
+
+    def move(self):
+        # loop in range start=2 -> 1 -> stop=0, step = -1
+        for dot in range(len(self.dots) - 1, 0, -1):
+            new_x = self.dots[dot - 1].xcor()    # pos x of 1
+            new_y = self.dots[dot - 1].ycor()    # pos y of 1
+            self.dots[dot].goto(new_x, new_y)    # move 2 -> 1
+
+        self.head.forward(MOVING_DISTANCE)  # move the head
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
